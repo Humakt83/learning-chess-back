8f2194c0d4760caa86eb960143ef9dc8d1d16603
Humakt83 <urlact@yahoo.com>
2015-09-01 23:54:33 +0300

    GameOverController with integration test

---
+++ src/main/java/fi/ukkosnetti/chess/controller/GameOverController.java
@@ -0,0 +1,25 @@
+package fi.ukkosnetti.chess.controller;
+
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import fi.ukkosnetti.chess.dto.GameResult;
+import fi.ukkosnetti.chess.service.BoardService;
+
+@Controller
+public class GameOverController {
+
+	@Autowired
+	private BoardService boardService;
+	
+	@RequestMapping(value = "/gameover", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON)
+	public @ResponseBody void getAIMove(@RequestBody GameResult gameResult) {
+		boardService.saveGameResult(gameResult);		
+	}
+}
---
+++ src/test/java/fi/ukkosnetti/chess/controller/GameOverControllerTest.java
@@ -0,0 +1,57 @@
+package fi.ukkosnetti.chess.controller;
+
+import static com.jayway.restassured.RestAssured.given;
+
+import java.util.Arrays;
+
+import javax.ws.rs.core.MediaType;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.jayway.restassured.RestAssured;
+
+import fi.ukkosnetti.chess.ChessApplication;
+import fi.ukkosnetti.chess.dto.GameResult;
+import fi.ukkosnetti.chess.test.util.BoardUtil;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ChessApplication.class)
+@WebAppConfiguration
+@IntegrationTest({"server.port:0",
+"spring.datasource.url:jdbc:h2:mem:learning-chess-back;DB_CLOSE_ON_EXIT=FALSE"})
+public class GameOverControllerTest {
+
+	@Value("${local.server.port}")
+	private int port;
+
+	@Before
+	public void setUp() throws Exception {
+		RestAssured.port = port;
+	}
+	
+	@Test
+	public void acceptsWhiteWin() {
+		given().contentType(MediaType.APPLICATION_JSON)
+			.body(new GameResult(Arrays.asList(BoardUtil.createStartingBoard(), BoardUtil.createEmptyBoard()), true))
+			.post("/gameover")
+			.then()
+			.statusCode(200);
+	}
+	
+	@Test
+	public void acceptsBlackWin() {
+		given().contentType(MediaType.APPLICATION_JSON)
+		.body(new GameResult(Arrays.asList(BoardUtil.createStartingBoard(), BoardUtil.createEmptyBoard()), false))
+		.post("/gameover")
+		.then()
+		.statusCode(200);
+	}
+	
+}
