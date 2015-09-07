27a77fd695a67237578455b0d3c856246826aa47
Humakt83 <urlact@yahoo.com>
2015-09-08 00:38:29 +0300

    Throw 404 when no legal moves remain

--- src/main/java/fi/ukkosnetti/chess/controller/ChessController.java
+++ src/main/java/fi/ukkosnetti/chess/controller/ChessController.java
@@ -3,10 +3,12 @@ package fi.ukkosnetti.chess.controller;
 import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import fi.ukkosnetti.chess.dto.Board;
@@ -25,6 +27,18 @@ public class ChessController {
 
 	@RequestMapping(value = "/aimove", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
 	public @ResponseBody Board getAIMove(@RequestBody Board board) {
-		return movePicker.getMove(board);
+		Board newBoard = movePicker.getMove(board);
+		if (newBoard == null) throw new ResourceNotFoundException("No legal moves left");
+		return newBoard;		
 	}
+		
+	@SuppressWarnings("serial")
+	@ResponseStatus(value = HttpStatus.NOT_FOUND)
+	class ResourceNotFoundException extends RuntimeException {
+
+		public ResourceNotFoundException(String message) {
+			super(message);
+		}
+	}
+	
 }
