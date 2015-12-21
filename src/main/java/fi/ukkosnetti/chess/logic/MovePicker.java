567d4c074c6eaa5d3165bc239cc96d9e1b6a3717
Humakt83 <urlact@yahoo.com>
2015-12-21 23:26:27 +0200

    Uses evaluated move now properly with random move

--- src/main/java/fi/ukkosnetti/chess/logic/MovePicker.java
+++ src/main/java/fi/ukkosnetti/chess/logic/MovePicker.java
@@ -1,8 +1,8 @@
 package fi.ukkosnetti.chess.logic;
 
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 import java.util.stream.Collectors;
 
@@ -16,7 +16,7 @@ import fi.ukkosnetti.chess.service.BoardService;
 
 @Component
 public class MovePicker {
-	
+
 	@Autowired
 	private BoardService boardService;
 
@@ -24,47 +24,55 @@ public class MovePicker {
 		List<Board> moves = generateMoves(board);
 		return moves.size() > 0 ? pickBestMove(moves, board.turnOfWhite) : null;
 	}
-	
+
 	private Board pickBestMove(List<Board> moves, boolean turnOfWhite) {
 		return turnOfWhite ? pickBestMoveForWhite(moves) : pickBestMoveForBlack(moves);
 	}
-	
-	private Board pickBestMoveForBlack(List<Board> moves) {
-		return moves.stream().map(board -> {
+
+	private Board pickBestMoveForBlack(final List<Board> moves) {
+		Optional<Board> bestMove =  moves.stream().map(board -> {
 			board.setValue(boardService.getBoardEntity(board.board).orElse(new BoardEntity()).getValue());
 			return board;
-		}).min(compareBoards()).orElse(pickEvaluatedMoveForBlack(moves));
+		}).min(compareBoards());
+		return bestMove.isPresent() && bestMove.get().getValue() < 0 ? bestMove.get() : pickEvaluatedMoveForBlack(moves);
 	}
 
 
-	private Board pickBestMoveForWhite(List<Board> moves) {
-		return moves.stream().map(board -> {
+	private Board pickBestMoveForWhite(final List<Board> moves) {
+		Optional<Board> bestMove =  moves.stream().map(board -> {
 			board.setValue(boardService.getBoardEntity(board.board).orElse(new BoardEntity()).getValue());
 			return board;
-		}).max(compareBoards()).orElse(pickEvaluatedMoveForWhite(moves));
+		}).max(compareBoards());
+		return bestMove.isPresent() && bestMove.get().getValue() > 0 ? bestMove.get() : pickEvaluatedMoveForWhite(moves);
 	}
-	
-	private Board pickEvaluatedMoveForBlack(List<Board> moves) {
-		System.out.println("Evaluating black");
-		return moves.stream()
+
+	private Board pickEvaluatedMoveForBlack(final List<Board> moves) {
+		final Long bestValue = moves.stream()
 				.map(BoardEvaluator::evaluateBoard)
 				.min(compareEvaluatedBoards())
-				.orElse(pickRandomMove(moves));
+				.get().getEvaluatedValue();
+		return pickRandomMoveFromBestEvaluatedMoves(moves, bestValue);
 	}
-	
-	private Board pickEvaluatedMoveForWhite(List<Board> moves) {
-		return moves.stream()
+
+	private Board pickEvaluatedMoveForWhite(final List<Board> moves) {
+		final Long bestValue = moves.stream()
 				.map(BoardEvaluator::evaluateBoard)
 				.max(compareEvaluatedBoards())
-				.orElse(pickRandomMove(moves));
+				.get().getEvaluatedValue();
+		return pickRandomMoveFromBestEvaluatedMoves(moves, bestValue);
 	}
-	
-	private Board pickRandomMove(List<Board> moves) {
-		System.out.println("Random");
+
+	private Board pickRandomMoveFromBestEvaluatedMoves(final List<Board> moves, final Long bestValue) {
+		return pickRandomMove(moves.stream()
+				.filter(board -> board.getEvaluatedValue().equals(bestValue))
+				.collect(Collectors.toList()));
+	}
+
+	private Board pickRandomMove(final List<Board> moves) {
 		Random random = new Random(System.currentTimeMillis());
 		return moves.get(random.nextInt(moves.size()));
 	}
-	
+
 	private List<Board> generateMoves(Board board) {
 		List<Piece> pieces = board.turnOfWhite ? getWhitePieces(board) : getBlackPieces(board);
 		return pieces.stream().map(piece -> piece.getMoves(board)).flatMap(move -> move.stream()).collect(Collectors.toList());
@@ -73,31 +81,17 @@ public class MovePicker {
 	private List<Piece> getBlackPieces(Board board) {
 		return MoveUtil.getPieces(board, false);
 	}
-	
+
 	private List<Piece> getWhitePieces(Board board) {
 		return MoveUtil.getPieces(board, true);
 	}
-	
+
 	private Comparator<? super Board> compareBoards() {
-		return (b1, b2) -> b1.getValue().compareTo(b2.getValue());
+		return (b1, b2) -> Long.compare(b1.getValue(), b2.getValue());
 	}
-	
+
 	private Comparator<? super Board> compareEvaluatedBoards() {
-		return (b1, b2) -> b1.getEvaluatedValue().compareTo(b2.getEvaluatedValue());
-	}
-	
-	public static void main(String ... args) {
-		MovePicker picker = new MovePicker();
-		List<Long> l = Arrays.asList(4l, 3l, 2l, -1l);
-		System.out.println(l.stream().max(Long::compareTo).orElse(null));
-		System.out.println(l.stream().min(Long::compareTo).orElse(null));
-		Board board = new Board(null, true);
-		board.setEvaluatedValue(400l);
-		Board board2 = new Board(null, true);
-		board2.setEvaluatedValue(-200l);
-		List<Board> boards = Arrays.asList(board, board2);
-		System.out.println(board == boards.stream().max(picker.compareEvaluatedBoards()).get());
-		System.out.println(board2 == boards.stream().min(picker.compareEvaluatedBoards()).get());
+		return (b1, b2) -> Long.compare(b1.getEvaluatedValue(), b2.getEvaluatedValue());
 	}
 
 }
