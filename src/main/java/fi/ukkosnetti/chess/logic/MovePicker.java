3c0c5212c0565234d79538d74f1efc9d6d4ac70c
Humakt83 <urlact@yahoo.com>
2015-12-21 23:42:53 +0200

    Organize imports

--- src/main/java/fi/ukkosnetti/chess/logic/MovePicker.java
+++ src/main/java/fi/ukkosnetti/chess/logic/MovePicker.java
@@ -2,7 +2,6 @@ package fi.ukkosnetti.chess.logic;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 import java.util.Random;
 import java.util.function.Function;
 import java.util.stream.Collectors;
